f49da70bb8756cfac8d50b4b6c4a6d0f65a5bd94
Santtu Lakkala <santtu.lakkala@unikie.com>
2024-10-24 11:35:39 +0300

    More refactoring
    Signed-off-by: Santtu Lakkala <santtu.lakkala@unikie.com>

--- src/data_provider.rs
+++ src/data_provider.rs
@@ -1,38 +1,50 @@
-use async_channel::Sender;
-use gio::ListStore;
-use glib::{IsA, Object};
-use gtk::{self, gio, glib, prelude::*};
 use std::cell::RefCell;
+use std::ops::Deref;
 use std::rc::Rc;
 use std::sync::{Arc, RwLock, RwLockReadGuard};
 use std::thread::{self, JoinHandle};
-use tokio::runtime::{Builder, Handle};
-use tokio::time::{timeout, Duration};
+use std::time::Duration;
 
+use gio::ListStore;
+use glib::{IsA, Object};
+use gtk::{self, gio, glib, prelude::*};
+
+use async_channel::Sender;
 use givc_client::{self, AdminClient};
+use givc_common::query::{Event, TrustLevel, VMStatus};
+use tokio::runtime::Builder;
 
 use crate::vm_gobject::VMGObject;
 //use crate::settings_gobject::SettingsGObject;//will be in use in the future
-use givc_common::query::{Event, TrustLevel, VMStatus};
 
 use crate::{ADMIN_SERVICE_ADDR, ADMIN_SERVICE_PORT};
 
 pub mod imp {
     use super::*;
 
-    trait TypedStore<T: IsA<Object>> {
-        fn get(&self, index: u32) -> Option<T>;
-        fn typed_iter(&self) -> impl Iterator<Item = T>;
-    }
+    #[derive(Debug, Clone)]
+    struct TypedListStore<T: IsA<Object>>(ListStore, std::marker::PhantomData<T>);
 
-    impl<T: IsA<Object>> TypedStore<T> for ListStore {
-        fn get(&self, index: u32) -> Option<T> {
+    impl<T: IsA<Object>> TypedListStore<T> {
+        pub fn new() -> Self {
+            Self(ListStore::new::<T>(), std::marker::PhantomData)
+        }
+
+        pub fn get(&self, index: u32) -> Option<T> {
             self.item(index).and_then(|item| item.downcast().ok())
         }
 
-        fn typed_iter(&self) -> impl Iterator<Item = T> {
+        fn iter(&self) -> impl Iterator<Item = T> {
             let s = self.clone();
             (0..).map_while(move |idx| s.get(idx))
+        }
+    }
+
+    impl<T: IsA<Object>> Deref for TypedListStore<T> {
+        type Target = ListStore;
+
+        fn deref(&self) -> &Self::Target {
+            &self.0
         }
     }
 
@@ -49,25 +61,24 @@ pub mod imp {
 
     #[derive(Debug)]
     pub struct DataProvider {
-        store: ListStore,
+        store: TypedListStore<VMGObject>,
         //settings: Arc<Mutex<SettingsGObject>>,//will be in use in the future
         pub status: bool,
         admin_client: Arc<RwLock<AdminClient>>,
         service_address: RefCell<(String, u16)>,
-        handle: Rc<RefCell<Option<Handle>>>,
         join_handle: RefCell<Option<JoinHandle<()>>>,
         task_runner: Rc<RefCell<Option<TaskSender>>>,
     }
 
     macro_rules! adminclient {
         (|$cl:ident| $block:expr) => {
-            Box::new(move |$cl| Box::pin(async move { $block.map_err(|e| e.to_string()) }))
+            Box::new(move |$cl| Box::pin(async move { $block.await.map_err(|e| e.to_string()) }))
         };
     }
 
     impl DataProvider {
         pub fn new(address: String, port: u16) -> Self {
-            let init_store = ListStore::new::<VMGObject>(); //Self::fill_by_mock_data();
+            let init_store = TypedListStore::new(); //Self::fill_by_mock_data();
 
             Self {
                 store: init_store,
@@ -76,7 +87,6 @@ pub mod imp {
                 admin_client: Arc::new(RwLock::new(AdminClient::new(address.clone(), port, None))),
                 service_address: RefCell::new((address, port)),
                 join_handle: RefCell::new(None),
-                handle: Rc::new(RefCell::new(None)),
                 task_runner: Rc::new(RefCell::new(None)),
             }
         }
@@ -88,7 +98,6 @@ pub mod imp {
                 self.service_address.borrow().1
             );
             let admin_client = self.admin_client.clone();
-            let _store = self.store.clone();
 
             let (event_tx, event_rx) = async_channel::unbounded();
             let (task_tx, task_rx) =
@@ -101,17 +110,30 @@ pub mod imp {
                     .unwrap()
                     .block_on(async move {
                         let timeout_duration = Duration::from_secs(5);
-                        let Ok(Ok(result)) =
-                            timeout(timeout_duration, admin_client.read().unwrap().watch()).await
-                        else {
-                            println!("Watch call timeout/error");
-                            return;
+                        let result = {
+                            let client = admin_client.read();
+                            tokio::select! {
+                                _ = tokio::time::sleep(timeout_duration) => {
+                                    println!("Watch call timeout");
+                                    return;
+                                },
+                                result = client.as_ref().unwrap().watch() => match result {
+                                    Ok(result) => result,
+                                    Err(e) => {
+                                        println!("Watch fall failed: {e}");
+                                        return;
+                                    }
+                                },
+                                _ = async {
+                                    while task_rx.recv().await.is_ok() {
+                                        println!("Not yet connected, task ignored");
+                                    }
+                                } => return,
+                            }
                         };
                         println!("Connected!");
 
-                        let _ = event_tx
-                            .send((result.channel, result.initial, Handle::current().clone()))
-                            .await;
+                        let _ = event_tx.send((result.channel, result.initial)).await;
                         while let Ok((task, resp)) = task_rx.recv().await {
                             let admin_client = admin_client.read().unwrap();
                             let res = task(admin_client).await;
@@ -121,16 +143,15 @@ pub mod imp {
             });
 
             *self.join_handle.borrow_mut() = Some(joinhandle);
+            *self.task_runner.borrow_mut() = Some(task_tx);
             let store = self.store.clone();
-            let selfhandle = self.handle.clone();
-            let taskrunner = self.task_runner.clone();
+            let task_runner = self.task_runner.clone();
             glib::spawn_future_local(async move {
-                let Ok((channel, initial, handle)) = event_rx.recv().await else {
+                let Ok((channel, initial)) = event_rx.recv().await else {
+                    task_runner.take();
                     return;
                 };
                 println!("Got stuff!");
-                *selfhandle.borrow_mut() = Some(handle);
-                *taskrunner.borrow_mut() = Some(task_tx);
                 store.remove_all();
                 for vm in initial {
                     store.append(&VMGObject::new(
@@ -145,19 +166,13 @@ pub mod imp {
                     match event {
                         Event::UnitStatusChanged(result) => {
                             println!("Status: {:?}", result);
-                            if let Some(obj) = store
-                                .typed_iter()
-                                .find(|obj: &VMGObject| obj.name() == result.name)
-                            {
+                            if let Some(obj) = store.iter().find(|obj| obj.name() == result.name) {
                                 obj.update(result);
                             }
                         }
                         Event::UnitShutdown(result) => {
                             println!("Shutdown info: {:?}", result);
-                            if let Some(obj) = store
-                                .typed_iter()
-                                .find(|obj: &VMGObject| obj.name() == result.name)
-                            {
+                            if let Some(obj) = store.iter().find(|obj| obj.name() == result.name) {
                                 obj.update(result);
                             }
                         }
@@ -172,6 +187,7 @@ pub mod imp {
                         }
                     }
                 }
+                task_runner.take();
             });
         }
 
@@ -201,11 +217,11 @@ pub mod imp {
         }
 
         pub fn get_store(&self) -> ListStore {
-            self.store.clone()
+            self.store.deref().clone()
         }
 
         pub fn get_current_service_address(&self) -> (String, u16) {
-            self.service_address.clone().into_inner()
+            self.service_address.borrow().clone()
         }
 
         pub fn set_service_address(&self, addr: String, port: u16) {
@@ -238,7 +254,6 @@ pub mod imp {
         pub fn disconnect(&self) {
             println!("Disconnect request...");
             self.task_runner.take();
-            self.handle.take();
             if let Some(joinhandle) = self.join_handle.take() {
                 joinhandle.join().unwrap();
                 //drop(self.admin_client.clone());
@@ -265,7 +280,7 @@ pub mod imp {
 
         pub fn start_vm(&self, name: String) {
             self.client_cmd(
-                adminclient!(|client| client.start(name, None, vec![]).await),
+                adminclient!(|client| client.start(name, None, vec![])),
                 |res| match res {
                     Ok(_) => println!("Start request sent"),
                     Err(error) => println!("Start request error {error}"),
@@ -274,18 +289,15 @@ pub mod imp {
         }
 
         pub fn pause_vm(&self, name: String) {
-            self.client_cmd(
-                adminclient!(|client| client.pause(name).await),
-                |res| match res {
-                    Ok(_) => println!("Pause request sent"),
-                    Err(error) => println!("Pause request error {error}"),
-                },
-            );
+            self.client_cmd(adminclient!(|client| client.pause(name)), |res| match res {
+                Ok(_) => println!("Pause request sent"),
+                Err(error) => println!("Pause request error {error}"),
+            });
         }
 
         pub fn resume_vm(&self, name: String) {
             self.client_cmd(
-                adminclient!(|client| client.resume(name).await),
+                adminclient!(|client| client.resume(name)),
                 |res| match res {
                     Ok(_) => println!("Resume request sent"),
                     Err(error) => println!("Resume request error {error}"),
@@ -294,13 +306,10 @@ pub mod imp {
         }
 
         pub fn shutdown_vm(&self, name: String) {
-            self.client_cmd(
-                adminclient!(|client| client.stop(name).await),
-                |res| match res {
-                    Ok(_) => println!("Stop request sent"),
-                    Err(error) => println!("Stop request error {error}"),
-                },
-            );
+            self.client_cmd(adminclient!(|client| client.stop(name)), |res| match res {
+                Ok(_) => println!("Stop request sent"),
+                Err(error) => println!("Stop request error {error}"),
+            });
         }
 
         pub fn restart_vm(&self, _name: String) {
@@ -311,7 +320,7 @@ pub mod imp {
 
         pub fn set_locale(&self, locale: String) {
             self.client_cmd(
-                adminclient!(|client| client.set_locale(locale).await),
+                adminclient!(|client| client.set_locale(locale)),
                 |res| match res {
                     Ok(_) => println!("Locale set"),
                     Err(e) => println!("Locale setting failed: {e}"),
@@ -321,7 +330,7 @@ pub mod imp {
 
         pub fn set_timezone(&self, timezone: String) {
             self.client_cmd(
-                adminclient!(|client| client.set_timezone(timezone).await),
+                adminclient!(|client| client.set_timezone(timezone)),
                 |res| match res {
                     Ok(_) => println!("Timezone set"),
                     Err(e) => println!("Timezone setting failed: {e}"),
